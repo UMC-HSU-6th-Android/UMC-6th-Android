4309c780f7ab7563404e197135ad316b772b0622
임창수 <imchangsu123@gmail.com>
2024-05-08 13:03:37 +0900

    Init: 초기 생성

---
+++ Kalt/MyApplication2/app/src/main/java/com/example/practice/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.example.practice
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.practice.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    lateinit var binding : ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        }
+    }
